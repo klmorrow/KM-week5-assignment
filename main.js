64667aac4d31d222ffbcaafdca6fa98f00fde54b
klmorrow <31272813+klmorrow@users.noreply.github.com>
2017-10-04 19:53:19 +1100

    Updated wk5

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 console.log('connected');
 $('#submit-btn').click(changeBackground)
 
+function removeBackground() {
+  $('body').removeClass('nyc');
+}
 function changeBackground(event) {
   event.preventDefault();
   var userInput = $('#city-type').val();
+  userInput = userInput.toLowerCase();
     if (userInput === 'nyc' || userInput === 'new york' || userInput === 'new york city') {
+      removeBackground();
       $('body').addClass('nyc');
       $('#city-type').val('');
   }
-    else if (userInput === 'SF' || userInput === 'San Francisco' || userInput === 'Bay Area') {
+    else if (userInput === 'sf' || userInput === 'san francisco' || userInput === 'bay area') {
+      removeBackground();
       $('body').addClass('sf');
       $('#city-type').val('');
     }
-    else if (userInput === 'Austin' || userInput === 'ATX') {
+    else if (userInput === 'austin' || userInput === 'atx') {
+      removeBackground();
       $('body').addClass('austin');
       $('#city-type').val('');
     }
-    else if (userInput === 'LA' || userInput === 'LAX' || userInput === 'Los Angeles') {
+    else if (userInput === 'la' || userInput === 'lax' || userInput === 'los angeles') {
+      removeBackground();
       $('body').addClass('la');
       $('#city-type').val('');
     }
-    else if (userInput === 'Sydney' || userInput === 'SYD') {
+    else if (userInput === 'sydney' || userInput === 'syd') {
+      removeBackground();
       $('body').addClass('sydney');
       $('#city-type').val('');
     }
